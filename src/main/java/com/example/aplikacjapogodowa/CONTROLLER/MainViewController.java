2a2b39910409b7f850db421c63c64c5a27985c7d
Karol <karojecz@gmail.com>
2023-07-15 14:18:53 +0200

    create Main Window

--- src/main/java/com/example/aplikacjapogodowa/CONTROLLER/MainViewController.java
+++ src/main/java/com/example/aplikacjapogodowa/CONTROLLER/MainViewController.java
@@ -1,4 +1,24 @@
 package com.example.aplikacjapogodowa.CONTROLLER;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MainViewController {
+
+    @FXML
+    private Button checkWeatherButton;
+
+    @FXML
+    private Label temperature;
+
+    @FXML
+    private Label teperatureLabel;
+
+    @FXML
+    void CheckWeatherAction() {
+        System.out.println("click!!");
+    }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.example.aplikacjapogodowa {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.aplikacjapogodowa to javafx.fxml;
+    opens com.example.aplikacjapogodowa.VIEW;
+    opens com.example.aplikacjapogodowa.CONTROLLER;
+    opens com.example.aplikacjapogodowa.MODEL;
     exports com.example.aplikacjapogodowa;
 }
