5d95290b0dd83b908168f37cfd46491ea7d2b84e
Karol <karojecz@gmail.com>
2023-07-16 16:13:10 +0200

    change weather client to interface

--- src/main/java/com/example/aplikacjapogodowa/CONTROLLER/MainViewController.java
+++ src/main/java/com/example/aplikacjapogodowa/CONTROLLER/MainViewController.java
@@ -1,10 +1,18 @@
 package com.example.aplikacjapogodowa.CONTROLLER;
 
+import com.example.aplikacjapogodowa.MODEL.Weather;
+import com.example.aplikacjapogodowa.MODEL.WeatherService;
+import com.example.aplikacjapogodowa.MODEL.WeatherServiceFactory;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class MainViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements FxmlDefinedController, Initializable {
+    private final String fxmlName;
 
     @FXML
     private Button checkWeatherButton;
@@ -14,11 +22,31 @@ public class MainViewController {
 
     @FXML
     private Label teperatureLabel;
+    private WeatherService weatherService;
+    public MainViewController(String fxmlName){this.fxmlName=fxmlName;}
 
     @FXML
     void CheckWeatherAction() {
         System.out.println("click!!");
+        String cityName="Krakow";
+        Weather weather=weatherService.getWeather(cityName);
+        displayWeather(weather);
     }
 
 
+    private void displayWeather(Weather weather) {
+        temperature.setVisible(true);
+        teperatureLabel.setVisible(true);
+        temperature.setText(" "+weather.getTempInCelcius());
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        weatherService= WeatherServiceFactory.createWeatherService();
+        temperature.setVisible(false);
+       teperatureLabel.setVisible(false);
+
+    }
 }
--- src/main/java/com/example/aplikacjapogodowa/MODEL/WeatherService.java
+++ src/main/java/com/example/aplikacjapogodowa/MODEL/WeatherService.java
@@ -1,4 +1,15 @@
 package com.example.aplikacjapogodowa.MODEL;
 
+import com.example.aplikacjapogodowa.MODEL.client.WeatherClient;
+
 public class WeatherService {
+    private final WeatherClient weatherClient;
+    public WeatherService(WeatherClient weatherClient){
+        this.weatherClient=weatherClient;
+    }
+    public Weather getWeather(String cityname){
+        return weatherClient.getWeather(cityname);
+    }
+
+
 }
