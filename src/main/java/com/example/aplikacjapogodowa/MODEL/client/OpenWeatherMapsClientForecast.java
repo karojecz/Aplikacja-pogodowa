7fe002109dacd7b6d8e5621c196a04ecade21f0a
Karol <karojecz@gmail.com>
2023-08-11 19:32:13 +0200

    add new class OpenWeatherMapsClientForecast

---
+++ src/main/java/com/example/aplikacjapogodowa/MODEL/client/OpenWeatherMapsClientForecast.java
@@ -0,0 +1,13 @@
+package com.example.aplikacjapogodowa.MODEL.client;
+
+import com.example.aplikacjapogodowa.MODEL.Weather;
+
+import java.io.IOException;
+
+public class OpenWeatherMapsClientForecast implements WeatherClient{
+
+    @Override
+    public Weather getWeather(String cityname, String countryName) throws IOException {
+        return null;
+    }
+}
